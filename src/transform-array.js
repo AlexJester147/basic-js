3ff4446cb996af5196b13d660a2776fb100a2589
AlexJester147 <vardy2016@lenta.ru>
2021-09-09 17:26:34 +0300

    feat : update

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,37 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ export default function transform(arr) {
+  let tran = 0;
+  for (let i=0; i < arr.length; i++){
+
+
+    if (arr === Array) {
+      throw new Error("'arr' parameter must be an instance of the Array!");
+    }
+
+    else {
+
+    
+
+    switch(arr[i]){
+      case '--double-prev':
+        arr[i]=arr[i-1];
+        break;
+      case '--double-next':
+        arr[i]=arr[i+1];
+        break;
+      case '--discard-next':
+        arr.splice(i,2);
+        break;
+      case '--discard-prev':
+        arr.splice(i-1,2);
+        break;
+    }
+  }
+
+  }
+
+return arr;
+
 }
